a8c23a42b6a0078eb6cdad946a06d9d70d4f0a41
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2016-02-23 20:50:55 +0100

    - update gitconfig - added time machine exclusions for homebrew

--- osx/timemachine.sh
+++ osx/timemachine.sh
@@ -4,3 +4,5 @@ sudo tmutil addexclusion $HOME/Library/Application\ Support/Steam/steamapps/
 sudo tmutil addexclusion $HOME/Movies
 sudo tmutil addexclusion $HOME/VirtualBox\ VMs
 sudo tmutil addexclusion /opt/homebrew-cask
+sudo tmutil addexclusion /Library/Caches/Homebrew
+sudo tmutil addexclusion /usr/local/Cellar/
