51b04226b60d2124bced400e9d00348e6052557e
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2019-09-11 09:15:48 +0200

    macOS fixes

--- bash/install.sh
+++ bash/install.sh
@@ -2,5 +2,3 @@ CURDIR=`pwd`
 # Link user profile
 ln -sfv "${CURDIR}/profile" ~/.bash_profile
 ln -sfv "${CURDIR}/profile" ~/.profile
-# Link .bashrc on mac
-[ $(uname -s) == "Darwin" ] && ln -sfv "${CURDIR}/bashrc" ~/.bashrc
